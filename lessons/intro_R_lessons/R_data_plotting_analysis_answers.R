edec02e07aa18edfddc9d28182683f51fe8cb07f
Mark Wilber <mqwilber@gmail.com>
2015-04-10 14:02:02 -0700

    Updated answer key

--- lessons/intro_R_lessons/R_data_plotting_analysis_answers.R
+++ lessons/intro_R_lessons/R_data_plotting_analysis_answers.R
@@ -9,9 +9,9 @@ element[4:1]
 # 2b. element[-4] is everything but element 4
 # 2c. element[-1:-4] returns ["e", "n"]
 
-# 3. 
+# 3.
 
-dat = read.csv("anuerysm_data.csv", header=TRUE)
+dat <- read.csv("aneurysm_data_site-1.csv", header=TRUE)
 index <- dat$Gender == 'm'
 dat[index,]
 
@@ -30,45 +30,47 @@ barplot(table(exprmt))
 
 ### Answers to Exercise 4 ###
 
-dat['Average_Anuerism'] = rowMeans(dat[, 6:9])
-write.csv(dat, "updated_aneurism.csv")
+dat['min_aneurysm'] <- apply(dat[, 6:9], 1, min)
+write.csv(dat, "updated_data.csv")
+new_dat <- read.csv("updated_data.csv", header=T)
+
 
 ### Answers for Exercises 5-8 ###
 
 clean_data <- function(data){
   # Clean a dataset
-  
+
   data$Gender[data$Gender == 'm'] = 'M'
   data$Gender[data$Gender == 'f'] = 'F'
   data <- droplevels(data)
   return(data)
-  
+
 }
 
 analyze <- function(filename){
   # A function that plots and analyzes aneurysm data
-  
+
   # 1. Load in data in filename and save it to a variable
   tdata <- read.csv(filename, header=TRUE)
-  
+
   # 2. Clean your data
   tdata <- clean_data(tdata)
-  
+
   # 3. Define a new response that is the mean number of anuerysms
-  tdata['avg_anuerism'] <- rowMeans(tdata[, 6:9])
-  
+  tdata['avg_an'] <- rowMeans(tdata[, 6:9])
+
   # 4. Visualize your data with a few plots
-  boxplot(avg_anuerism ~ Gender, data=tdata, main=filename)
-  
+  boxplot(avg_an ~ Gender, data=tdata, main=filename)
+
   # 5. Run an analysis for each dataset and output results
-  fit <- lm(avg_anuerism ~ Gender + Group + BloodPressure, data=tdata)
+  fit <- lm(avg_an ~ Gender + Group + BloodPressure, data=tdata)
   results <- anova(fit)
   return(results)
-  
+
 }
 
 # Loop over all five datasets
-all_files <- list.files(pattern="anuerysm")
+all_files <- list.files(pattern="aneurysm")
 for(filename in all_files){
   print(analyze(filename))
 }
